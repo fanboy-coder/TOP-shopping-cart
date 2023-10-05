e897f4c813ae2aaf84b6405edc4336c7fecf5de2
Fanboy Coder <joel.monteiro.ferreira@gmail.com>
2023-10-05 11:52:51 +0100

    Finished the application. Working on CSS.

--- src/pages/ShoppingCart.jsx
+++ src/pages/ShoppingCart.jsx
@@ -17,47 +17,57 @@ const ShoppingCart = () => {
 	}, [cart]);
 
 	const handleCartChange = (item, newQuantity) => {
-		const itemIndex = cart.findIndex((cartItem) =>cartItem.id === item.id);
+		const itemIndex = cart.findIndex((cartItem) => cartItem.id === item.id);
 
-		if(item.id !== -1) {
+		if (item.id !== -1) {
 			const updatedCart = [...cart];
 			updatedCart[itemIndex].quantity = newQuantity;
 			setCart(updatedCart);
 		}
 	}
 
+	const handleDelete = (title) => {
+		const updatedCart = cart.filter((titles) => titles.title !== title);
+		setCart(updatedCart);
+		console.log(cart)
+	}
+
 	return (
-		<div className="grid shopping-grid">
+		<div className="grid">
 			<div className="page-title">
 				<h2>Shopping Cart:</h2>
 			</div>
 			<ul className="order-grid">
 				{cart.map((item) => (
-					<li key={item.id} className="item-box">
+					<li key={item.id} className="item-box cart">
+						<div className="item-picture">
+							<img src={item.image} alt="" />
+						</div>
 						<div className="item-info">
-							<div className="item-picture">
-								<img src={item.image} alt="" />
+							<div className="top-level-info">
+								<div className="item-title">{item.title}</div>
+								<div>
+									<button className="close-button" onClick={() => handleDelete(item.title)}>X</button>
+								</div>
 							</div>
-							<div className="item-title">
-								<div>{item.title}</div>
-							</div>
-							<div>
+							<div className="low-level-info">
+								<p>Quantity: </p>
 								<QuantityDropdown
 									onChange={(newQuantity) =>
-									handleCartChange(item, newQuantity)
-								}
-								value={item.quantity}
+										handleCartChange(item, newQuantity)
+									}
+									value={item.quantity}
 								/>
 							</div>
-						</div>
-						<div className="item-total">
-							<div><p>Total: {(item.price * item.quantity).toFixed(2)}</p></div>
+							<div className="item-total">
+								<p>Total: {(item.price * item.quantity).toFixed(2)}</p>
+							</div>
 						</div>
 					</li>
 				))}
 			</ul>
 			<div className="page-title">
-				<h2>Total:{total}</h2>
+				<h2>Cart total: {total}</h2>
 			</div>
 		</div>
 	)
